5bd10f57dd9ececd4fde5b6145bce865a6aa75c8
gierr7976 <gierr7976@gmail.com>
2022-05-22 01:45:54 +0300

    ref: VisitorScope

--- lib/src/navigation/case/visitor.dart
+++ lib/src/navigation/case/visitor.dart
@@ -1,48 +1,30 @@
 part of dialog_bot.navigation.usecase;
 
-class VisitorState {
-  final Visitor visitor;
-
-  String get route => visitor.route;
-
-  const VisitorState({
-    required this.visitor,
-  });
-
-  VisitorState copyWith({
-    Visitor? visitor,
-  }) =>
-      VisitorState(
-        visitor: visitor ?? this.visitor,
-      );
-}
-
-class VisitorScope extends Cubit<VisitorState?> {
+class VisitorScope extends Cubit<Visitor?> {
+  @protected
   late final VisitorRepository _repository;
 
-  VisitorState get ready => state!;
+  Visitor get ready => state!;
 
   VisitorScope() : super(null);
 
   Future<void> init() async => _repository = await GetIt.instance.getAsync();
 
-  @mustCallSuper
-  Future<Visitor> checkout(int id) async {
+  Future<void> checkout(int id) async {
     Visitor? visitor = await _repository.fetch(id);
 
-    return visitor ?? _getFallbackVisitor(id);
+    emit(
+      visitor ?? _getFallbackVisitor(id),
+    );
   }
 
-  @mustCallSuper
-  FutureOr<void> next(String route) async => emit(
-        ready.copyWith(
-          visitor: ready.visitor.copyWith(
-            route: route,
-          ),
-        ),
-      );
+  FutureOr<void> next(String route) async {
+    final Visitor visitor = ready.copyWith(route: route);
 
-  Future<void> saveRoute() async => _repository.updateRoute(ready.visitor);
+    await _repository.updateRoute(visitor);
+
+    emit(visitor);
+  }
 
   Visitor _getFallbackVisitor(int id) => Visitor(
         key: ObjectId(),
