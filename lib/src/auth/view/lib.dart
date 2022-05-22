d44a6265d9d3b5d3dfa2d3a0cb8e5e8d85116393
gierr7976 <gierr7976@gmail.com>
2022-05-22 14:18:53 +0300

    ref: auth.view

--- lib/src/auth/view/lib.dart
+++ lib/src/auth/view/lib.dart
@@ -4,6 +4,8 @@ import 'package:dialog_bot/src/auth/case/lib.dart';
 import 'package:dialog_bot/src/core/view/lib.dart';
 import 'package:dialog_bot/src/navigation/view/lib.dart';
 
-part 'sentinel.dart';
-part 'provider.dart';
 part 'listener.dart';
+
+part 'provider.dart';
+
+part 'sentinel.dart';
