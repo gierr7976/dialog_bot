fcd458cb8272f162e37de907616c101e37b2eda3
gierr7976 <gierr7976@gmail.com>
2022-05-23 21:18:56 +0300

    feat: BackPoint

--- lib/src/home/view/menu.dart
+++ lib/src/home/view/menu.dart
@@ -17,3 +17,37 @@ abstract class MenuPoint extends FlowPoint with Stored {
 
   ReplyMarkup keyboard(BotUser user);
 }
+
+class BackPoint extends MenuPoint {
+  static KeyboardButton buildButton() => KeyboardButton(text: kButton);
+
+  static const String kName = 'back';
+
+  static const String kButton = 'Назад';
+
+  @override
+  String get button => kButton;
+
+  @override
+  String get name => kName;
+
+  final FlowPoint preferred;
+
+  final Map<String, FlowPoint> routes;
+
+  BackPoint({
+    required this.preferred,
+    this.routes = const {},
+  });
+
+  @override
+  FutureOr<void> forUser(BotUser user) {
+    final String route = user.route;
+
+    return navigator.next(routes[route] ?? preferred);
+  }
+
+  @override
+  ReplyMarkup keyboard(BotUser user) =>
+      throw UnsupportedError('BackPoint should not have keyboard');
+}
