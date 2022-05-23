be4ef0ff68096aae412fb6b3dd0bdd145039263c
gierr7976 <gierr7976@gmail.com>
2022-05-23 23:28:29 +0300

    feat: VisitorRepository

--- lib/src/core/repo/lib.dart
+++ lib/src/core/repo/lib.dart
@@ -1,6 +1,7 @@
 library dialog_bot.core.repo;
 
 import 'package:dialog_bot/src/core/debug/lib.dart';
+import 'package:dialog_bot/src/core/entity/lib.dart';
 import 'package:dialog_bot/src/gen/config.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
@@ -10,3 +11,5 @@ export 'package:mongo_dart/mongo_dart.dart';
 
 part 'mongo.dart';
 part 'repository.dart';
+
+part 'visitor.dart';
---
+++ lib/src/core/repo/visitor.dart
@@ -0,0 +1,30 @@
+part of dialog_bot.core.repo;
+
+abstract class VisitorRepository {
+  Future<Visitor?> fetch(int id);
+
+  Future<void> store(Visitor visitor);
+}
+
+class MongoVisitorRepository extends MongoRepository
+    implements VisitorRepository {
+  static const String collection = 'visitors';
+
+  MongoVisitorRepository(super.service);
+
+  @override
+  Future<Visitor?> fetch(int id) async {
+    final Map<String, dynamic>? raw = await db.collection(collection).findOne(
+      {'id': id},
+    );
+
+    if (raw is Map<String, dynamic>) return Visitor.fromJson(raw);
+
+    return null;
+  }
+
+  @override
+  Future<void> store(Visitor visitor) => db.collection(collection).insertOne(
+        visitor.toJson(),
+      );
+}
