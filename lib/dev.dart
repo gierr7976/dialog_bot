f04550a3313b2c633317a7dd8feca92e23c76f9a
gierr7976 <gierr7976@gmail.com>
2022-05-25 21:42:59 +0300

    feat: Development bot

--- lib/dev.dart
+++ lib/dev.dart
@@ -1,26 +1,17 @@
+import 'package:dialog_bot/src/core/view/lib.dart';
 import 'package:dialog_bot/src/dependencies.dart';
+import 'package:dialog_bot/src/gen/config.dart';
+import 'package:dialog_bot/src/start/view/lib.dart';
 
 void main() async {
   Dependencies.config();
-/*
+
   final FlowBot bot = FlowBot(
     token: BotConfig.token,
-    listeners: [
-      StartListener(),
-      LetsGoListener(),
-      HomeListener(),
-    ],
-    publicCommands: [
-      BotCommand(
-        command: StartListener.command,
-        description: 'Запустить бота',
-      ),
-      BotCommand(
-        command: HomeListener.command,
-        description: 'Домашняя страница',
-      ),
+    roots: [
+      StartCommand(),
     ],
   );
 
-  await bot.start();*/
+  await bot.start();
 }
