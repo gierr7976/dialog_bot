2be3d5b74a27344c2427070b9fccd0e1d112fe4d
gierr7976 <gierr7976@gmail.com>
2022-05-25 07:27:39 +0300

    feat: user model

---
+++ lib/src/user/entity/access.dart
@@ -0,0 +1,52 @@
+part of dialog_bot.user.entity;
+
+@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+class Position {
+  final String name;
+  final List<Permission> permissions;
+
+  const Position({
+    required this.name,
+    required this.permissions,
+  });
+
+  //<editor-fold desc="Data class methods">
+
+  Position copyWith({
+    String? name,
+    List<Permission>? permissions,
+  }) =>
+      Position(
+        name: name ?? this.name,
+        permissions: permissions ?? this.permissions,
+      );
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Position &&
+          runtimeType == other.runtimeType &&
+          name == other.name;
+
+  @override
+  int get hashCode => name.hashCode;
+
+  //</editor-fold>
+
+  //<editor-fold desc="Serialization">
+
+  factory Position.fromJson(
+    Map<String, dynamic> json,
+  ) =>
+      _$PositionFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PositionToJson(this);
+
+//</editor-fold>
+}
+
+@JsonEnum(fieldRename: FieldRename.snake)
+enum Permission {
+  all,
+  invite,
+}
---
+++ lib/src/user/entity/user.dart
@@ -0,0 +1,48 @@
+part of dialog_bot.user.entity;
+
+@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+class DialogUser extends Visitor {
+  final String fullName;
+
+  final List<Position> positions;
+
+  const DialogUser({
+    required super.key,
+    required super.id,
+    required super.route,
+    required this.fullName,
+    required this.positions,
+  });
+
+  //<editor-fold desc="Data class methods">
+
+  @override
+  DialogUser copyWith({
+    int? id,
+    Uri? route,
+    ObjectId? key,
+    String? fullName,
+    List<Position>? positions,
+  }) =>
+      DialogUser(
+        id: id ?? this.id,
+        route: route ?? this.route,
+        key: key ?? this.key,
+        fullName: fullName ?? this.fullName,
+        positions: positions ?? this.positions,
+      );
+
+  //</editor-fold>
+
+  //<editor-fold desc="Serialization">
+
+  factory DialogUser.fromJson(
+    Map<String, dynamic> json,
+  ) =>
+      _$DialogUserFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => _$DialogUserToJson(this);
+
+//</editor-fold>
+}
